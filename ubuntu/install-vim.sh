77c5727537af8f1b25c6653669393a72fe94e8e7
Klesh Wong <klesh@qq.com>
2020-06-11 12:00:49 +0800

    [feature] replace vim with neovim

--- ubuntu/install-vim.sh
+++ ubuntu/install-vim.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-sudo add-apt-repository ppa:jonathonf/vim -y
-sudo apt update
-sudo apt install -y vim
+#sudo add-apt-repository ppa:jonathonf/vim -y
+#sudo apt update
+#sudo apt install -y vim
+
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt-get update
+sudo apt install -y neovim
