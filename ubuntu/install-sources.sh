76ff14bc26a6fe962c691623f6bae66d84d0c137
Klesh Wong <klesh@qq.com>
2020-07-15 11:16:54 +0800

    [feature] add pip mirror and docker accelerator

--- ubuntu/install-sources.sh
+++ ubuntu/install-sources.sh
@@ -11,3 +11,21 @@ if [ ! -e /etc/apt/sources.bak ]; then
   SOURCE_CODENAME=$(head -n 1 $DIR/sources.$ARCH.list | cut -d' ' -f3)
   sudo bash -c "sed s/$SOURCE_CODENAME/$CURRENT_CODENAME/g $DIR/sources.$ARCH.list > /etc/apt/sources.list"
 fi
+
+# pip mirrors
+mkdir -p $HOME/.pip
+cat <<EOF > $HOME/.pip/pip.conf
+[global]
+#trusted-host =  mirrors.aliyun.com
+#index-url = https://mirrors.aliyun.com/pypi/simple
+index-url = https://pypi.doubanio.com/simple
+trusted-host = pypi.doubanio.com
+EOF
+
+# docker accelerator
+if [ -d /etc/docker ]; then$
+  dj='{}'$
+  [ -f /etc/docker/daemon.json ] && dj=$(cat /etc/docker/daemon.json)$
+  echo $dj | jq '. + {"registry-mirrors": ["https://izuhlbap.mirror.aliyuncs.com"]}' > /etc/docker/daemon.json$
+  systemctl restart docker$
+fi$
