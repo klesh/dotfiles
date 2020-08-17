bc999776e7d79c4ca64353f80ead3b245e7edac0
Klesh Wong <klesh@qq.com>
2020-08-17 11:20:23 +0800

    [bugfix] replace yarn with yarnpkg

--- bin/07-nodejs.fish
+++ bin/07-nodejs.fish
@@ -2,13 +2,13 @@
 
 
 sudo pacman -S nodejs yarn
-yarn config set registry https://registry.npm.taobao.org --global  && \
-yarn config set disturl https://npm.taobao.org/dist --global && \
-yarn config set sass_binary_site https://npm.taobao.org/mirrors/node-sass --global  && \
-yarn config set electron_mirror https://npm.taobao.org/mirrors/electron/ --global  && \
-yarn config set puppeteer_download_host https://npm.taobao.org/mirrors --global  && \
-yarn config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver --global  && \
-yarn config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver --global  && \
-yarn config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs --global  && \
-yarn config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium --global  && \
-yarn config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector --global
+yarnpkg config set registry https://registry.npm.taobao.org --global  && \
+yarnpkg config set disturl https://npm.taobao.org/dist --global && \
+yarnpkg config set sass_binary_site https://npm.taobao.org/mirrors/node-sass --global  && \
+yarnpkg config set electron_mirror https://npm.taobao.org/mirrors/electron/ --global  && \
+yarnpkg config set puppeteer_download_host https://npm.taobao.org/mirrors --global  && \
+yarnpkg config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver --global  && \
+yarnpkg config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver --global  && \
+yarnpkg config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs --global  && \
+yarnpkg config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium --global  && \
+yarnpkg config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector --global
