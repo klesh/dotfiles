30a66f153abaeed67c4fbccf83560e54807f883e
Klesh Wong <klesh@qq.com>
2020-09-07 15:28:10 +0800

    ...

--- ubuntu/install-nodejs.sh
+++ ubuntu/install-nodejs.sh
@@ -7,17 +7,20 @@ rm -rf /tmp/node
 mkdir -p /tmp/node
 tar xf /tmp/node.tar.xz --strip 1 -C /tmp/node
 pushd /tmp/node
-cp -r bin include lib share /usr/local
+sudo cp -r bin include lib share /usr/local
 popd
 rm -rf /tmp/node*
-npm install -g yarn
-yarn config set registry https://registry.npm.taobao.org --global  && \
-yarn config set disturl https://npm.taobao.org/dist --global && \
-yarn config set sass_binary_site https://npm.taobao.org/mirrors/node-sass --global  && \
-yarn config set electron_mirror https://npm.taobao.org/mirrors/electron/ --global  && \
-yarn config set puppeteer_download_host https://npm.taobao.org/mirrors --global  && \
-yarn config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver --global  && \
-yarn config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver --global  && \
-yarn config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs --global  && \
-yarn config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium --global  && \
-yarn config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector --global
+sudo apt install yarnpkg
+yarnpkg config set registry https://registry.npm.taobao.org --global  && \
+yarnpkg config set disturl https://npm.taobao.org/dist --global && \
+yarnpkg config set sass_binary_site https://npm.taobao.org/mirrors/node-sass --global  && \
+yarnpkg config set electron_mirror https://npm.taobao.org/mirrors/electron/ --global  && \
+yarnpkg config set puppeteer_download_host https://npm.taobao.org/mirrors --global  && \
+yarnpkg config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver --global  && \
+yarnpkg config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver --global  && \
+yarnpkg config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs --global  && \
+yarnpkg config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium --global  && \
+yarnpkg config set sqlite3_binary_host_mirror "https://foxgis.oss-cn-shanghai.aliyuncs.com/" --global  && \
+yarnpkg config set profiler_binary_host_mirror "https://npm.taobao.org/mirrors/node-inspector/" --global  && \
+yarnpkg config set chromedriver_cdnurl "https://cdn.npm.taobao.org/dist/chromedriver" --global  && \
+yarnpkg config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector --global
