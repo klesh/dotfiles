0ccea9505a537c2bf822e4b39b20329399b7b5fd
Klesh Wong <klesh@qq.com>
2020-09-18 00:11:58 +0800

    [misc] nextcloud installation on ubuntu

--- office/nextcloud.sh
+++ office/nextcloud.sh
@@ -9,9 +9,11 @@ case "$PM" in
     apt)
         echo TODO
         exit -1
+        sudo add-apt-repository ppa:nextcloud-devs/client
+        sudo apt-get update
+        sudo apt install -y nextcloud-client
         ;;
     pacman)
-        sudo pacman -S \
-            nextcloud-client
+        sudo pacman -S nextcloud-client
         ;;
 esac
