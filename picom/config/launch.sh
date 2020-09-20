a8b371da081432db59079021bf99f962843983cb
Klesh Wong <klesh@qq.com>
2020-09-20 17:57:43 +0800

    [bugfix] monotor restoration took too long of time

--- picom/config/launch.sh
+++ picom/config/launch.sh
@@ -4,5 +4,5 @@ DIR=$(readlink -f $(dirname $0))
 
 killall -q picom
 while pgrep -x picom >/dev/null; do sleep 1; done
-picom -b --config $DIR/picom.conf --experimental-backends --blur-method dual_kawase --blur-strength 6
+picom -b --config $DIR/picom.conf --experimental-backends --blur-method dual_kawase --blur-strength 6 --no-fading-openclose
 #picom -b --config $DIR/picom.conf
