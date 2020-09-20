17c97c8a60cb024a34c9aff6826c0722feea265d
Klesh Wong <klesh@qq.com>
2020-09-20 11:40:50 +0800

    [bugfix] in case of ~/.xinitrc is symlink

--- suckless/install.sh
+++ suckless/install.sh
@@ -53,6 +53,7 @@ cd ~/Projects/suckless/slock && sudo rm -f config.h && sudo make clean install
 cd ~/Projects/suckless/dwm && sudo rm -f config.h && sudo make clean install
 
 # config xinit to start for dwm
+rm ~/.xinitrc
 cat <<EOT > ~/.xinitrc
 export QT_QPA_PLATFORMTHEME="qt5ct"
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
