7ef5ce462ada8257ec4552a38a26c7c1f3c77221
Klesh Wong <klesh@qq.com>
2020-09-22 12:26:27 +0800

    [bugfig] ibus not working on google-chrome

--- suckless/install.sh
+++ suckless/install.sh
@@ -59,8 +59,8 @@ export QT_QPA_PLATFORMTHEME="qt5ct"
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 
 export XMODIFIERS="@im=ibus"
-export QT_IM_MODULE=ibus
-export GTK_IM_MODULE=ibus
+export QT_IM_MODULE=xim
+export GTK_IM_MODULE=xim
 
 
 # xrandr --setprovideroutputsource modesetting NVIDIA-0
