3a96b498997062437cfcc72cbeab798a3596b563
Klesh Wong <klesh@qq.com>
2020-09-22 09:52:02 +0800

    [misc] move py3 to optim folder

---
+++ python/py3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# install python3.8
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.8
+
+# install pip and other distutils
+sudo apt install python3.8-distutils
+sudo python3.8 -m pip install --upgrade pip setuptools wheel
