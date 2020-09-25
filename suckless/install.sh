d85fd2488c73f47f16df23c54a22b634bd92db2f
Klesh Wong <klesh@qq.com>
2020-09-25 15:48:37 +0800

    [bugfix] ibus not working for keepassxc

--- suckless/install.sh
+++ suckless/install.sh
@@ -59,7 +59,7 @@ export QT_QPA_PLATFORMTHEME="qt5ct"
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 
 export XMODIFIERS="@im=ibus"
-export QT_IM_MODULE=xim
+export QT_IM_MODULE=ibus
 export GTK_IM_MODULE=xim
 
 
