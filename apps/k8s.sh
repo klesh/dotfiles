b246e7dc4403d17877c5b3ac4f8ba529e68f3a49
Klesh Wong <klesh@qq.com>
2020-10-10 14:50:55 +0800

    [feature] install kubectl and its fish completion

---
+++ apps/k8s.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR=$(readlink -f $(dirname $0))
+. $DIR/../env.sh
+
+
+# install docker
+case "$PM" in
+    apt)
+        #sudo snap install kubectl --classic
+        ;;
+    pacman)
+        # TODO
+        ;;
+esac
+
+# completion for fish
+case "$DEFAULT_SHELL" in
+    "$FISH")
+        [[ ! -f $HOME/.config/fish/completions/docker.fish ]] && \
+            curl -Lo $HOME/.config/fish/completions/docker.fish --create-dirs \
+            'https://github.com/docker/cli/raw/master/contrib/completion/fish/docker.fish'
+        fish -c "fisher add evanlucas/fish-kubectl-completions"
+        ;;
+esac
