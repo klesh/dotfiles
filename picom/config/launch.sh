03ab4c8b884581b26d5ca0a68b52deb20df4175c
Klesh Wong <klesh@qq.com>
2020-11-04 14:12:26 +0800

    [bugfix] mitigate picom exit with zero when experimental backends failed

--- picom/config/launch.sh
+++ picom/config/launch.sh
@@ -6,5 +6,6 @@ killall -q picom
 while pgrep -x picom >/dev/null; do sleep 1; done
 picom -b --config $DIR/picom.conf --experimental-backends \
     --blur-method dual_kawase --blur-strength 6 \
-    --no-fading-openclose || \
-picom -b --config $DIR/picom.conf --no-fading-openclose
+    --no-fading-openclose
+sleep 1
+! pgrep -x picom && picom -b --config $DIR/picom.conf --no-fading-openclose
