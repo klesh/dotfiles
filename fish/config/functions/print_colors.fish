ac47877adad8cf28263aa6de7a84da0c0dd4b15c
Klesh Wong <klesh@qq.com>
2020-11-19 17:27:51 +0800

    [misc] move print_colors to bin folder

--- fish/config/functions/print_colors.fish
+++
@@ -1,8 +0,0 @@
-
-function print_colors -d 'print out all terminal colors'
-    set x (tput op)
-    set y (printf '%76s')
-    for i in (seq 256)
-        printf "%3s %s%s%s%s\n" $i (tput setaf $i) (tput setab $i) $y $x
-    end
-end
