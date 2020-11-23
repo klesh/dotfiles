6a3e3ca3fb3cd0310202079543e4ba02a39df263
Klesh Wong <klesh@qq.com>
2020-11-23 12:12:55 +0800

    [feature] single page markdown composing

--- devel/cmark-gfm.sh
+++ devel/cmark-gfm.sh
@@ -7,11 +7,18 @@ DIR=$(dirname "$(readlink -f "$0")")
 # install cmark-gfm: convert markdown to html,man,xml,latex,comman-mark
 case "$PM" in
     apt)
-        sudo apt install -y cmark-gfm
+        sudo apt install -y cmark-gfm entr
         ;;
     pacman)
-        sudo pacman -S --noconfirm --needed cmark-gfm
+        sudo pacman -S --noconfirm --needed cmark-gfm entr
         ;;
 esac
 
-# convert to html with embeded <style>: cmark-gfm input.md --to html --unsafe
+# live server for html previewing
+. "$PDIR/devel/nodejs.sh"
+sudo $NPM_BIN install -g live-server
+
+# convert to html with embeded <style>: cmark-gfm input.md --to html --unsafe > /path/to/output.html
+# entr watch input.md and rerun convert command
+# live-server to observe html file and reload browser
+
