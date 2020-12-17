92d217ba35ad8da27d31846aac642fcf0f1b50f6
Klesh Wong <klesh@qq.com>
2020-12-17 15:18:43 +0800

    [bugfix] typo

--- cli/vim.sh
+++ cli/vim.sh
@@ -50,7 +50,7 @@ if ! has_cmd "$VIM"; then
 fi
 
 # symlink configuration
-sudo ln -sf "$(command -v nvim)" /usr/bin/v
+sudo ln -sf "$(command -v vim)" /usr/bin/v
 lnsf "$DIR/vim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
 lnsf "$DIR/vim/coc-settings.json" "$XDG_CONFIG_HOME/nvim/coc-settings.json"
 lnsf "$DIR/vim/coc-settings.json" "$HOME/.vim/coc-settings.json"
