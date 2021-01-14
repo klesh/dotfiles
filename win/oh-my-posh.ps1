121072c540f9a74b1f29ec751b283f09448552b6
Klesh Wong <klesh@kleshwong.com>
2021-01-14 12:56:57 +0800

    [feature] add fzf

--- win/oh-my-posh.ps1
+++ win/oh-my-posh.ps1
@@ -1,6 +1,10 @@
-# Install-Module -Name PowerShellGet -Force -AllowPrerelease
-# Install-Module -Name posh-git
-# Install-Module -Name oh-my-posh
+# curl:     https://curl.se/windows/
+# ag:       https://github.com/k-takata/the_silver_searcher-win32/releases
+# fzf:      https://github.com/junegunn/fzf/releases/latest
+Install-Module -Name PowerShellGet -Force -AllowPrerelease
+Install-Module -Name posh-git
+Install-Module -Name oh-my-posh
+Install-Module -Name psfzf
 
 $dotfiles=(Get-Item $PSScriptRoot).Parent.FullName
 New-Item -ItemType SymbolicLink -Target $dotfiles\win\profile.ps1 -Path $profile -Force
