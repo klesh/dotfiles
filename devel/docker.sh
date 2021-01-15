8d6a7f15c6cfb1e6969d352272db269d041ec132
Klesh Wong <klesh@kleshwong.com>
2021-01-15 16:05:06 +0800

    [bugfix] jq should be installed beforehand

--- devel/docker.sh
+++ devel/docker.sh
@@ -10,7 +10,7 @@ case "$PM" in
         # snap docker will intefere native docker.io, must be dealt with
         sudo snap remove --purge docker
         pm_update
-        sudo apt install -y docker.io
+        sudo apt install -y docker.io jq
         "$PDIR/devel/python.sh"
         sudo pip3 install docker-compose
         ;;
@@ -19,8 +19,6 @@ case "$PM" in
         ;;
 esac
 
-sudo systemctl enable docker
-sudo systemctl start docker
 
 # configuration
 sudo usermod -aG docker "$USER"
@@ -43,3 +41,6 @@ if in_china; then
         sudo tee /etc/docker/daemon.json
     sudo systemctl restart docker
 fi
+
+sudo systemctl enable docker
+sudo systemctl start docker
