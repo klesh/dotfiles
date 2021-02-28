2e873564c65e2dd1c799f09758b770bf7bd802cb
Klesh Wong <klesh@kleshwong.com>
2021-02-28 12:33:19 +0800

    [feature] mkdir / copy fullpath / copy file name

---
+++ win/setup/lf.ps1
@@ -0,0 +1,3 @@
+
+$dotfiles=(Get-Item $PSScriptRoot).Parent.Parent.FullName
+New-Item -Force -ItemType SymbolicLink -Target $dotfiles\cli\lf\lfrc.win -Path $Env:LOCALAPPDATA\lf\lfrc
--- win/setup/windows-terminal.ps1
+++ win/setup/windows-terminal.ps1
@@ -1,3 +1,3 @@
 
 $dotfiles=(Get-Item $PSScriptRoot).Parent.Parent.FullName
-New-Item -Force -ItemType SymbolicLink -Target $dotfiles\WindowsTerminal\settings.json -Path $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+New-Item -Force -ItemType SymbolicLink -Target $dotfiles\win\WindowsTerminal\settings.json -Path $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
