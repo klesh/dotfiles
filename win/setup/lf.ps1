6f7f711d31cea8d25d4b95530f0d230c5108a1c5
Klesh Wong <klesh@qq.com>
2021-06-03 22:40:24 +0800

    [bugfix] lf: disable icons, add download link

--- win/setup/lf.ps1
+++ win/setup/lf.ps1
@@ -1,3 +1,4 @@
+# https://github.com/gokcehan/lf/releases/latest
 
 $dotfiles=(Get-Item $PSScriptRoot).Parent.Parent.FullName
 New-Item -Force -ItemType SymbolicLink -Target $dotfiles\cli\lf\lfrc.win -Path $Env:LOCALAPPDATA\lf\lfrc
