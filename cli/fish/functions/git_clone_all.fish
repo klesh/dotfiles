530c743a4b5c82bd09178ce6e0ddfff1fcb9dd6c
Klesh Wong <klesh@klesh-laptop.localdomain>
2021-06-19 17:42:39 +0800

    ...

--- cli/fish/functions/git_clone_all.fish
+++ cli/fish/functions/git_clone_all.fish
@@ -1,6 +1,8 @@
 function git_clone_all
-    for remote_branch in (git branch -r | awk 'NR>1{print $1}')
+    for remote_branch in (git branch -r | awk '{print $1}')
         set local_branch (basename $remote_branch)
-        git branch --track $local_branch $remote_branch
+        if not git rev-parse --verify "$local_branch"
+            git branch --track $local_branch $remote_branch
+        end
     end
 end
