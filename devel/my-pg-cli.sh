199cee2945cfdedf63b196a82ccea9e073ffb394
Klesh Wong <klesh@qq.com>
2021-10-23 11:22:19 +0800

    feat: add script to install mycli/pgcli

--- devel/my-pg-cli.sh
+++ devel/my-pg-cli.sh
@@ -4,5 +4,19 @@ DIR=$(dirname "$(readlink -f "$0")")
 . "$DIR/../env.sh"
 "$PDIR/devel/python.sh"
 
+# install mycli and pgcli
+case "$PM" in
+    pkg)
+        echo todo
+        exit -1
+        ;;
+    apt)
+        sudo apt install -y libpq-dev
+        ;;
+    pacman)
+        sudo pacman -S --noconfirm --needed postgresql-libs
+        ;;
+esac
+
 sudo pip install mycli
 sudo pip install pgcli
