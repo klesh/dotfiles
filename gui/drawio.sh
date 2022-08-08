e450731fbe88b24f0b9ed33c8670c4ba2d83c008
Klesh Wong <klesh@qq.com>
2022-08-08 11:24:25 +0800

    feat: open *.drawio file with drawio

--- gui/drawio.sh
+++ gui/drawio.sh
@@ -11,4 +11,16 @@ echo download icon from github and save to ~/.local/share/icons/drawio.svg
 xdg-open "https://github.com/jgraph/drawio-desktop/blob/dev/build/icon.svg"
 
 
+cat <<MIME > ~/.local/share/mime/packages/drawio.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+    <mime-type type="application/vnd.jgraph.mxfile">
+        <comment>Drawio Diagram</comment>
+        <glob pattern="*.drawio" case-sensitive="true"/>
+    </mime-type>
+</mime-info>
+MIME
+
 lnsf "$DIR/drawio/drawio.desktop" ~/.local/share/applications/drawio.desktop
+
+update-mime-database ~/.local/share/mime
