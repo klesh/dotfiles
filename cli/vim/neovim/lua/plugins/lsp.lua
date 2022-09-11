c35040650cb44aa7c985be5cf1416251eb26d3ce
Klesh Wong <klesh@qq.com>
2022-09-11 13:11:36 +0800

    feats: golang linting for nvim

--- cli/vim/neovim/lua/plugins/lsp.lua
+++ cli/vim/neovim/lua/plugins/lsp.lua
@@ -146,3 +146,21 @@ end
 --         },
 --     })
 -- end
+
+-- golangci
+local configs = require 'lspconfig/configs'
+if not configs.golangcilsp then
+    configs.golangcilsp = {
+        default_config = {
+            cmd = { 'golangci-lint-langserver' },
+            root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+            init_options = {
+                command = { "golangci-lint", "run", "--out-format", "json",
+                    "--issues-exit-code=1" };
+            }
+        };
+    }
+end
+lspconfig.golangci_lint_ls.setup {
+    filetypes = { 'go', 'gomod' }
+}
